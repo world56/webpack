76e8cfd47f032e4393b8d9aa0872888ebc96e67c
junrui.hjr <junrui.hjr@dtwave-inc.com>
2020-10-15 21:01:26 +0800

    add

--- config/production.js
+++ config/production.js
@@ -1,4 +1,5 @@
 const { resolve } = require("path");
+// const {deleteFolder} = require('../script/build')
 const devServer = require("./devServer");
 // 打包html
 const HTMLWebpackPlugin = require("html-webpack-plugin");
@@ -22,19 +23,64 @@ module.exports = {
   // loader配置 通常用于解析相关文件
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        // exclude: /\.json$/, // 暂时不处理json
+        loader: "babel-loader",
+        options: {
+          // js语法兼容到 es5
+          // @babel/preset-env 只做基本语法兼容
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                // 按需加载
+                useBuiltIns: "usage",
+                corejs: {
+                  version: 3,
+                },
+                // 指定兼容性做到哪个版本浏览器
+                targets: {
+                  chrome: "60",
+                  firefox: "60",
+                  ie: "9",
+                },
+              },
+            ],
+          ],
+          // 插件还能以沙箱垫片的方式防止污染全局
+          //抽离公共的 helper function , 以节省代码的冗余
+          plugins: [
+            "@babel/plugin-transform-runtime",
+            "@babel/plugin-transform-modules-commonjs"
+          ],
+        },
+      },
+
+      // eslint 校验
+      {
+        test: /\.js$/,
+        // 排除 对/node_modules/的检查
+        exclude: "/node_modules/",
+        loader: "eslint-loader",
+        options: {
+          // 自动修复
+          fix: true,
+        },
+      },
+
       // 解析css
       {
         // test 匹配格式 这里一般用正则进行匹配
         test: /\.css$/,
         // 使用 加载顺序是倒叙加载的
         use: [
-          // 直接使用 style-loader 是直接打包进js，然后动态加入style标签
-          // "style-loader",
           // 这里尝试打包成静态css文件
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
               esModule: false,
+              publicPath: "../",
             },
           },
           // 将css文件解析成js字符
@@ -52,16 +98,26 @@ module.exports = {
           },
         ],
       },
+
+      // 解析less 并打包进入至js 可以动态引入html style标签
       {
         test: /\.less$/,
         use: [
-          // 直接打包进入css
-          "style-loader",
+          // 直接打包进入js 并且动态通过js创建stlye标签引入
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+            },
+          },
+          // "style-loader",
+          // 解析成可识别css代码
           "css-loader",
           // 配置第三方css解析 应该先解析成css在插入成style标签
           "less-loader",
         ],
       },
+
       {
         // 处理多个文件
         // warning 处理不了html元素src引入的图片 用html-loader可以解决
@@ -81,6 +137,7 @@ module.exports = {
           outputPath: "img",
         },
       },
+
       {
         // url-loader默认使用es6模块解析 html-loader是用commonJS解析
         // 所以需要关闭url-loader的es模块化规范
@@ -89,6 +146,7 @@ module.exports = {
           loader: "html-loader",
         },
       },
+
       // 排除其他资源(除了html css js资源的其他资源)
       {
         exclude: /\.(css|js|html|jpg|png|jpeg|gif|json|less)$/,
@@ -114,10 +172,10 @@ module.exports = {
       filename: "./css/[hash:10].css",
     }),
     // 压缩
-    new OptimizeCssAssetsWebpackPlugin({}),
+    new OptimizeCssAssetsWebpackPlugin(),
   ],
   // 当前模式
   // 开发:development  生产：production
-  mode: "production",
+  mode: "development",
   devServer,
 };
--- src/index.js
+++ src/index.js
@@ -7,15 +7,30 @@
  * webpack ./src/index.js -o ./build/built.js --mode=production
  * @warning webpack不能对css、img进行识别 需要配置loader
  */
-import json from './json.json'
-import './index.css'
-import './index.less'
+// @babel/polyfill 能兼容一些更新的语法 例如promise 但是引入体积会过大
 
-import './resource/iconfont/iconfont.css'
+import json from './json.json';
+import './index.css';
+import './index.less';
+import './resource/iconfont/iconfont.css';
 
 function add() {
-    console.log('JSON122@', json)
-    return 1 + 2
+  // eslint-disable-next-line
+  console.log("JSON122@", json);
+  return 1 + 2;
 }
 
-add()
+add();
+
+const App = function App() {
+  return 1;
+};
+
+function PromiseTest() {
+  return new Promise((reslove) => {
+    reslove();
+  });
+}
+
+App();
+PromiseTest();
